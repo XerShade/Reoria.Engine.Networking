f195ab10aa0f02fe44b896e1c5fdcd7b64c9ff23
XerShade <xershade.ca@gmail.com>
2025-04-23 17:36:54 -0400

    feat: add tls WelcomePacket

---
+++ Packets/WelcomePacket.cs
@@ -0,0 +1,22 @@
+﻿using LiteNetLib.Utils;
+using Reoria.Engine.Networking.Packets.Interface;
+
+namespace Reoria.Engine.Networking.Packets;
+
+public class WelcomePacket : IPacket
+{
+    public Guid PlayerGuid = Guid.Empty;
+    public string UdpSessionKey = string.Empty;
+
+    public void Serialize(NetDataWriter writer)
+    {
+        writer.Put(this.PlayerGuid.ToString());
+        writer.Put(this.UdpSessionKey);
+    }
+
+    public void Deserialize(NetDataReader reader)
+    {
+        this.PlayerGuid = Guid.Parse(reader.GetString());
+        this.UdpSessionKey = reader.GetString();
+    }
+}
