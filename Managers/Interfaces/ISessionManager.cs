1d42121d51afc85cf6f3d5dc35baedb209223593
XerShade <xershade.ca@gmail.com>
2025-05-24 13:33:24 -0400

    feat: added basic session manager

---
+++ Managers/Interfaces/ISessionManager.cs
@@ -0,0 +1,10 @@
+﻿using Reoria.Engine.Networking.Sessions;
+
+namespace Reoria.Engine.Networking.Managers.Interfaces;
+
+public interface ISessionManager
+{
+    void CheckSessionExpiry();
+    void CloseSession(SocketSession session);
+    SocketSession OpenSession();
+}
---
+++ Managers/SessionManager.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Logging;
+using Reoria.Engine.Networking.Managers.Interfaces;
+using Reoria.Engine.Networking.Sessions;
+
+namespace Reoria.Engine.Networking.Managers;
+
+public class SessionManager(ILogger<ISessionManager> logger) : ISessionManager
+{
+    protected readonly ILogger<ISessionManager> Logger = logger;
+    protected readonly List<SocketSession> Sessions = [];
+    protected readonly Lock @lock = new();
+
+    public virtual SocketSession OpenSession()
+    {
+        SocketSession session = new();
+
+        this.Sessions.Add(session);
+
+        return session;
+    }
+
+    public virtual void CloseSession(SocketSession session)
+    {
+        _ = this.Sessions.Remove(session);
+        session.Close();
+    }
+
+    public virtual void CheckSessionExpiry()
+    {
+        foreach (SocketSession session in this.Sessions.ToArray())
+        {
+            if (session.IsExpired)
+            {
+                this.CloseSession(session);
+            }
+        }
+    }
+}
